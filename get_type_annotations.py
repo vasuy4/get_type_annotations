aa6be3af6794d929f83430b7c21df8752a893176
Артём Рыжов <ar.ryqp@mail.ru>
2024-09-30 12:03:11 +0300

    add check empty object2

--- get_type_annotations.py
+++ get_type_annotations.py
@@ -12,23 +12,20 @@ def get_type_annotation(obj: Any) -> str:
     if type(obj) is dict:
         set_types_keyes: Set[str] = {get_type_annotation(k) for k in obj.keys()}
         set_types_values: Set[str] = {get_type_annotation(v) for v in obj.values()}
-        len_keys: int = len(set_types_keyes)
-        len_values: int = len(set_types_values)
-        if len_keys > 1:
-            result += f"Dict[Union[{", ".join(set_types_keyes)}], "
-        elif len_keys == 1:
+        if len(set_types_keyes) > 1:
+            result += f"Dict[Union[{", ".join(set_types_keyes if set_types_keyes else None)}], "
+        else:
             result += f"Dict[{set_types_keyes.pop()}, "
-        if len_values > 1:
+        if len(set_types_values) > 1:
             result += f"Union[{", ".join(set_types_values)}]]"
-        elif len_values == 1:
-            result += f"{set_types_values.pop()}]"
+        else:
+            result += f"{set_types_values.pop() if set_types_values else None}]"
     elif type(obj) is list:
         set_types: Set[str] = {get_type_annotation(v) for v in obj}
-        len_set: int = len(set_types)
-        if len_set > 1:
+        if len(set_types) > 1:
             result += f"List[Union[{", ".join(set_types)}]]"
-        elif len_set == 1:
-            result += f"List[{set_types.pop()}]"
+        else:
+            result += f"List[{set_types.pop() if set_types else None}]"
     else:
         return str(type(obj).__name__)
     return result
