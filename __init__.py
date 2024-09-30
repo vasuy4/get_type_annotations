42bf1260a5f251240192ff52e1874cfd3c2f088c
Артём Рыжов <ar.ryqp@mail.ru>
2024-09-30 11:12:42 +0300

    init commut

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from . import get_type_annotations
---
+++ get_type_annotations.py
@@ -0,0 +1,31 @@
+from typing import Dict, Union, Set, Any, List
+
+
+def get_type_annotation(obj: Any) -> str:
+    """
+    Возвращает строку с аннотацией типа для переданного объекта.
+
+    :param obj: Объект, для которого нужно определить аннотацию типа.
+    :return: Строка с аннотацией типа в формате Dict[str, Union[...]]
+    """
+    result: str = ""
+    if type(obj) is dict:
+        set_types_keyes: Set[str] = {get_type_annotation(k) for k in obj.keys()}
+        set_types_values: Set[str] = {get_type_annotation(v) for v in obj.values()}
+        if len(set_types_keyes) > 1:
+            result += f"Dict[Union[{", ".join(set_types_keyes)}], "
+        else:
+            result += f"Dict[{set_types_keyes.pop()}, "
+        if len(set_types_values) > 1:
+            result += f"Union[{", ".join(set_types_values)}]]"
+        else:
+            result += f"{set_types_values.pop()}]"
+    elif type(obj) is list:
+        set_types: Set[str] = {get_type_annotation(v) for v in obj}
+        if len(set_types) > 1:
+            result += f"List[Union[{", ".join(set_types)}]]"
+        else:
+            result += f"List[{set_types.pop()}]"
+    else:
+        return str(type(obj).__name__)
+    return result
